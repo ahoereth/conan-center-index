750279ae37376efc134ffaed3ebe31f3e9318854
Alexander Höreth <a.hoereth@gmail.com>
2026-02-06 22:14:44 +0000

    protobuf: Provide protoc from build-context for cross-building.

--- recipes/protobuf/all/protobuf-conan-protoc-target.cmake
+++ recipes/protobuf/all/protobuf-conan-protoc-target.cmake
@@ -1,17 +1,19 @@
 if(NOT TARGET protobuf::protoc)
     # Locate protoc executable
+    ## First check the package folder (handles both native builds and cross-builds
+    ## where protoc was copied from the build-context package)
+    set(_PROTOC_PACKAGE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../bin/protoc${CMAKE_EXECUTABLE_SUFFIX}")
+    if(EXISTS "${_PROTOC_PACKAGE_PATH}")
+        set(PROTOC_PROGRAM "${_PROTOC_PACKAGE_PATH}")
+    endif()
+    unset(_PROTOC_PACKAGE_PATH)
     ## Workaround for legacy "cmake" generator in case of cross-build
-    if(CMAKE_CROSSCOMPILING)
+    if(NOT PROTOC_PROGRAM AND CMAKE_CROSSCOMPILING)
         find_program(PROTOC_PROGRAM NAMES protoc PATHS ENV PATH NO_DEFAULT_PATH)
     endif()
-    ## And here this will work fine with "CMakeToolchain" (for native & cross-build)
-    ## and legacy "cmake" generator in case of native build
+    ## Fallback: standard find_program
     if(NOT PROTOC_PROGRAM)
         find_program(PROTOC_PROGRAM NAMES protoc)
-    endif()
-    ## Last resort: we search in package folder directly
-    if(NOT PROTOC_PROGRAM)
-        set(PROTOC_PROGRAM "${CMAKE_CURRENT_LIST_DIR}/../../../bin/protoc${CMAKE_EXECUTABLE_SUFFIX}")
     endif()
     get_filename_component(PROTOC_PROGRAM "${PROTOC_PROGRAM}" ABSOLUTE)
 
